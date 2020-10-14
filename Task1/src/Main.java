e81943cb79c365bcb1a6469b5eb65430f9efccdb
Gleb <gslepenkov@gmail.com>
2020-10-14 16:41:05 +0300

    Code reformatting done.

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -4,38 +4,36 @@ import java.util.*;
 
 public class Main {
 
-    public static Long inputSumValue(InputStream stream){
+    public static Long inputSumValue(InputStream stream) {
         Scanner in = new Scanner(stream);
         String input;
         System.out.print("Введите желаемую сумму: ");
         input = in.nextLine();
         long sum;
         try {
-
-            sum =  handleInput(input.trim().split("\\s+")[0]);
+            sum = handleInput(input.trim().split("\\s+")[0]);
             return sum;
-
         } catch (NumberFormatException e) {
             throw new NumberFormatException("Введено некорректное значение для суммы");
-
         }
 
     }
 
-    public static Long[] inputNominals( InputStream stream) {
+    public static Long[] inputNominals(InputStream stream) {
         SortedSet<Long> cashData = new TreeSet<>();
         Scanner in = new Scanner(stream);
 
         System.out.print("Введите номиналы: ");
-        String [] inputArray = in.nextLine().trim().split("\\s+");
+        String[] inputArray = in.nextLine().trim().split("\\s+");
         in.close();
         for (String s : inputArray) {
             try {
                 long inputVal = handleInput(s);
-                if (inputVal == 0)
+                if (inputVal == 0) {
                     System.out.println("0 - недопустимое значение номинала. Оно будет пропущено");
-                else
+                } else {
                     cashData.add(inputVal);
+                }
             } catch (NumberFormatException e) {
                 System.out.println("Введены запрещенные символы. Значение проигнорированно");
 
@@ -47,43 +45,45 @@ public class Main {
 
     public static Long handleInput(String input) {
         long ans;
-
-        if (input.matches("[+-]?\\d+\\.\\d*"))
+        if (input.matches("[+-]?\\d+\\.\\d*")) {
             ans = Long.parseLong((input.split("\\."))[0]);
-        else if (input.matches("[+-]?\\d+,\\d*"))
+        } else if (input.matches("[+-]?\\d+,\\d*")) {
             ans = Long.parseLong((input.split(","))[0]);
-        else if (input.matches("-\\d*"))
+        } else if (input.matches("-\\d*")) {
             ans = Long.parseLong(input);
-        else
+        } else {
             return Long.parseLong(input);
+        }
 
         if (Long.signum(ans) == -1) {
             System.out.println("Было введено отрицательное число. Программа будет работать с его модулем.");
             return Math.abs(ans);
-        } else
+        } else {
             return ans;
+        }
     }
 
-    public static long exchangeWays(long sum, Long[] cashData, Long[] currentCombination,  int recLevel) {
+    public static long exchangeWays(long sum, Long[] cashData, Long[] currentCombination, int recLevel) {
         if (recLevel == 0) {
             if (sum % cashData[0] == 0) {
                 currentCombination[0] = sum / cashData[0];
                 System.out.print("Новая комбинация: ");
                 for (int i = 0; i < cashData.length; i++) {
-                    if(currentCombination[i]>0){
+                    if (currentCombination[i] > 0) {
                         System.out.print(currentCombination[i]);
-                        if(currentCombination[i]%10==1)
+                        if (currentCombination[i] % 10 == 1) {
                             System.out.print(" купюра номиналом ");
-                        else if (currentCombination[i]%10 >=2 && currentCombination[i]%10 <=4)
+                        } else if (currentCombination[i] % 10 >= 2 && currentCombination[i] % 10 <= 4) {
                             System.out.print(" купюры номиналом ");
-                        else
+                        } else {
                             System.out.print(" купюр номиналом ");
-                        System.out.print(cashData[i]+"; ");
+                        }
+                        System.out.print(cashData[i] + "; ");
                     }
                 }
                 System.out.println("\n");
                 return 1;
-            }else
+            } else
                 return 0;
         } else {
             long diffCoeff = sum / cashData[recLevel];
@@ -91,7 +91,7 @@ public class Main {
             currentCombination[recLevel] = diffCoeff;
             for (long i = currentCombination[recLevel]; i >= 0; i--) {
                 currentCombination[recLevel] = i;
-                thisLevelCombination += exchangeWays((sum - i * cashData[recLevel]),cashData, currentCombination, recLevel - 1);
+                thisLevelCombination += exchangeWays((sum - i * cashData[recLevel]), cashData, currentCombination, recLevel - 1);
             }
             return thisLevelCombination;
         }
@@ -101,9 +101,9 @@ public class Main {
 
         long sum = inputSumValue(System.in);
         Long[] cashData = inputNominals(System.in);
-        if (cashData.length == 0 || sum==0)
-            System.out.println("Не существует способов размена суммы " + sum + " номиналами "+Arrays.toString(cashData));
-        else {
+        if (cashData.length == 0 || sum == 0) {
+            System.out.println("Не существует способов размена суммы " + sum + " номиналами " + Arrays.toString(cashData));
+        } else {
             int size = cashData.length;
 
             Long[] currentCombination = new Long[size];
@@ -111,8 +111,8 @@ public class Main {
                 currentCombination[i] = (long) 0;
             }
 
-           long amOfComb = exchangeWays(sum, cashData, currentCombination, size - 1);
-            System.out.println("Итого, существует  " + amOfComb + " способов разменять сумму номиналами "+ Arrays.toString(cashData));
+            long amOfComb = exchangeWays(sum, cashData, currentCombination, size - 1);
+            System.out.println("Итого, существует  " + amOfComb + " способов разменять сумму номиналами " + Arrays.toString(cashData));
         }
     }
 }
--- Task1/test/MainTest.java
+++ Task1/test/MainTest.java
@@ -12,20 +12,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @ParameterizedTest
-    @ValueSource(strings = {"123456", "123456.12","123456,12","123456 789111","-123456", "-123456.12","-123456,12","-123456 789111"})
+    @ValueSource(strings = {"123456", "123456.12", "123456,12", "123456 789111", "-123456", "-123456.12", "-123456,12", "-123456 789111"})
     void testCorrectSumInput(String str) {
         InputStream stream = new ByteArrayInputStream(str.getBytes());
         Long actRes = Main.inputSumValue(stream);
-        Assertions.assertEquals(123456,actRes);
+        Assertions.assertEquals(123456, actRes);
     }
-    
-    /*
-    @ParameterizedTest
-    @ValueSource(strings = {"a123456", "12.3456.12","123 456,12","assdf 789111","null"})
-    void incorrectSumInput(String str) {
-        InputStream stream = new ByteArrayInputStream(str.getBytes());
 
-    }*/
 
     @ParameterizedTest
     @ValueSource(strings = {"1 2 3 4 5 6", "1 2.0 3,5 -4,56 5.00000 -6"})
@@ -33,24 +26,23 @@ class MainTest {
         InputStream stream = new ByteArrayInputStream(str.getBytes());
         Long[] actRes = Main.inputNominals(stream);
         Long[] expResult = new Long[6];
-        for(int i = 0;i<=5;i++)
-        {
-            expResult[i]= (long)(i+1);
+        for (int i = 0; i <= 5; i++) {
+            expResult[i] = (long) (i + 1);
         }
-        Assertions.assertArrayEquals(expResult,actRes);
+        Assertions.assertArrayEquals(expResult, actRes);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"12345", "12345,6","-12345", "12345.6"})
+    @ValueSource(strings = {"12345", "12345,6", "-12345", "12345.6"})
     void testHandleInputWithCorrectStrings(String Strings) {
         Long actualResult = Main.handleInput(Strings);
-        Assertions.assertEquals(12345,actualResult);
+        Assertions.assertEquals(12345, actualResult);
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"abcabc", "123q23",".....", "123,23,123"})
+    @ValueSource(strings = {"abcabc", "123q23", ".....", "123,23,123"})
     void testHandleInputWithIncorrectStrings(String Strings) {
-       Assertions.assertThrows(NumberFormatException.class, ()-> Main.handleInput(Strings));
+        Assertions.assertThrows(NumberFormatException.class, () -> Main.handleInput(Strings));
     }
 
 
@@ -58,8 +50,8 @@ class MainTest {
     void testExchangeWays() {
         long sum = 5;
         Long[] longCashData = new Long[2];
-        for(int i =0;i<2;i++){
-            longCashData[i]=(long)(i+1);
+        for (int i = 0; i < 2; i++) {
+            longCashData[i] = (long) (i + 1);
         }
 
         int size = longCashData.length;
@@ -67,7 +59,7 @@ class MainTest {
         for (int i = 0; i < size; i++) {
             currentCombination[i] = (long) 0;
         }
-        Assertions.assertEquals(3,Main.exchangeWays(sum,longCashData,currentCombination,size-1));
+        Assertions.assertEquals(3, Main.exchangeWays(sum, longCashData, currentCombination, size - 1));
     }
 
 }
