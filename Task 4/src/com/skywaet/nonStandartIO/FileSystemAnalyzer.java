953737e32130e303f6acdd75c6820fe48174430b
Gleb <gslepenkov@gmail.com>
2020-11-11 19:58:28 +0300

    New Version Creation Process

--- Task 4/src/com/skywaet/nonStandartIO/FileSystemAnalyzer.java
+++ Task 4/src/com/skywaet/nonStandartIO/FileSystemAnalyzer.java
@@ -1,32 +1,65 @@
 package com.skywaet.nonStandartIO;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
 
 public class FileSystemAnalyzer {
-    public static void directoryAnalyzer(File dir,int level) {
-        if (dir.isDirectory()) {
-            try {
-                for (File item : dir.listFiles()) {
+    File dir;
+    File outputFile;
 
-                    if (item.isDirectory()) {
-                        System.out.println("\uD83D\uDCC1 " + item.getName());
-                        directoryAnalyzer(item,level+1);
-
-                    } else {
-                        System.out.println(item.getName());
-                    }
-                }
-            }catch (NullPointerException e){
-                System.out.println(e);
-            }
-
+    public FileSystemAnalyzer(String dirPath, String FilePath) throws NoSuchFileException {
+        File dir = new File(dirPath);
+        if (dir.exists()) {
+            this.dir = dir;
+        } else {
+            throw new NoSuchFileException("Directory '"+dir+ "' not found");
+        }
+        File outputFile = new File(FilePath);
+        if (outputFile.exists()) {
+            this.outputFile = outputFile;
+        } else {
+            throw new NoSuchFileException("File '"+outputFile+ "' not found");
         }
     }
 
+    public void directoryAnalyser(File dir) {
+        if (dir.isDirectory()) {
+            try {
+                for (File item : dir.listFiles()) {
+                    if (item.isDirectory()) {
+                        FileWriter outStream = new FileWriter(this.outputFile, true);
+                        outStream.write("\uD83D\uDCC1 " + item.getName() + '\n');
+                        outStream.close();
+                        directoryAnalyser(item);
 
-    public static void main(String[] args) {
-        File dir = new File("D:\\учеба");
-        // если объект представляет каталог
-        directoryAnalyzer(dir,0);
+                    } else {
+                        FileWriter outStream = new FileWriter(this.outputFile, true);
+                        outStream.write(item.getName() + '\n');
+                        outStream.close();
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String @NotNull [] args) throws IOException {
+        File dir = new File(args[0]);//.getCanonicalFile();
+        System.out.println(args[0]);
+        File outputFile = new File(args[1]).getCanonicalFile();
+
+/*        if (outputFile.exists()) {
+            outputFile.delete();
+        }
+        if (dir.exists()) {
+            directoryAnalyser(dir, outputFile);
+        } else {
+            System.out.println("Directory not found");
+        }*/
     }
 }
---
+++ Task 4/test/com/skywaet/nonStandartIO/FileSystemAnalyzerTest.java
@@ -0,0 +1,12 @@
+package com.skywaet.nonStandartIO;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileSystemAnalyzerTest {
+    @Test
+    void directoryAnalyser() {
+
+    }
+}
